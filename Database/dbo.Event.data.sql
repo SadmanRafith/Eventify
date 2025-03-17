c415c37ec7887d04926fcb017714fe754157ac56
Sadman Rafith <108008599+SadmanRafith@users.noreply.github.com>
2025-03-17 12:50:36 +0600

    Add files via upload

---
+++ Database/dbo.Event.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[Event] ON
+INSERT INTO [dbo].[Event] ([eId], [title], [category], [capacity], [regesterd_seats], [price], [venue], [date], [start_time], [duration], [image1], [image2]) VALUES (0, N'aaaa', N'aaaa', N'1111', 0, 1111, N'aaaa', N'2023-11-29 10:34:28', N'Nov 29 2023 10:34AM', N'aaaa', <Binary Data>, <Binary Data>)
+SET IDENTITY_INSERT [dbo].[Event] OFF
---
+++ Database/dbo.Table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Event]
+(
+	[eId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [title] NVARCHAR(50) NULL, 
+    [category] NVARCHAR(50) NULL, 
+    [capacity] NVARCHAR(50) NULL, 
+    [regesterd_seats] INT NULL DEFAULT 0, 
+    [price] INT NULL, 
+    [venue] NVARCHAR(50) NULL, 
+    [date] DATETIME NULL, 
+    [start_time] NVARCHAR(50) NULL, 
+    [duration] NVARCHAR(50) NULL, 
+    [image1] NVARCHAR(50) NULL
+)
